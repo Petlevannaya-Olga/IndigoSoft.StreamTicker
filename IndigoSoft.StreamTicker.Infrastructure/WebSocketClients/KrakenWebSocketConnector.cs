a77d96e80cd1f768df8058ee61ad01e91a99e54f
Petlevannaya-Olga <olga.g.afonina@gmail.com>
2026-03-27 11:23:04 +0300

    KrakenWebSocketConnector

--- IndigoSoft.StreamTicker.Infrastructure/WebSocketClients/KrakenWebSocketConnector.cs
+++ IndigoSoft.StreamTicker.Infrastructure/WebSocketClients/KrakenWebSocketConnector.cs
@@ -1,10 +1,12 @@
 ﻿using System.Net.WebSockets;
+using System.Text;
+using System.Text.Json;
 using IndigoSoft.StreamTicker.Application;
 using Microsoft.Extensions.Logging;
 
 namespace IndigoSoft.StreamTicker.Infrastructure.WebSocketClients;
 
-public class KrakenWebSocketConnector( ILogger<KrakenWebSocketConnector> logger) : IWebSocketConnector
+public class KrakenWebSocketConnector(string[] symbols, ILogger<KrakenWebSocketConnector> logger) : IWebSocketConnector
 {
     public async Task<ClientWebSocket> ConnectAsync(Uri uri, CancellationToken ct)
     {
@@ -12,5 +14,22 @@ public class KrakenWebSocketConnector( ILogger<KrakenWebSocketConnector> logger)
         await ws.ConnectAsync(uri, ct);
         logger.LogInformation("Connected to {Uri}", uri);
         return ws;
+    }
+
+    private async Task SubscribeAsync(ClientWebSocket ws, CancellationToken ct)
+    {
+        // Подписка на сделки для нескольких пар
+        var subscribeMsg = new
+        {
+            @event = "subscribe",
+            pair = symbols,
+            subscription = new { name = "trade" }
+        };
+
+        await ws.SendAsync(
+            Encoding.UTF8.GetBytes(JsonSerializer.Serialize(subscribeMsg)),
+            WebSocketMessageType.Text,
+            true,
+            ct);
     }
 }
